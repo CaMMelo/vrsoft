7ded4efecced659b583cf057fe7ee112a7643404
cmelo <cmelo.marcelo.pereira@gmail.com>
2025-08-25 08:47:45 -0300

    unit test

--- tests/test_entrada_callback.py
+++ tests/test_entrada_callback.py
@@ -1,6 +1,27 @@
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, MagicMock
+from uuid import uuid4
 
-from backend.model import notificar
+import pytest
+
+from backend.model import PayloadNotificacao, TipoNotificacao, notificar
 
 
-async def test_callback_entrada(): ...
+@pytest.mark.asyncio
+async def test_notificar():
+    channel = MagicMock()
+    channel.default_exchange.publish = AsyncMock()
+
+    payload = PayloadNotificacao(
+        mensagemId=uuid4(),
+        conteudoMensagem="Mensagem de teste",
+        tipoNotificacao=TipoNotificacao.EMAIL,
+    )
+
+    notificacoes = {}
+
+    notificacao = await notificar(payload, channel, notificacoes)
+
+    assert notificacoes[notificacao.traceId] is notificacao
+    assert payload.mensagemId == notificacao.mensagemId
+    assert payload.conteudoMensagem == notificacao.conteudoMensagem
+    assert payload.tipoNotificacao == notificacao.tipoNotificacao
